5266d4f8b653fdb5271ab57049a15fcdfd9ce83a
MrJuise <MrJuise@mail.ru>
2025-01-17 13:11:36 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils import executor
-from handlers import *
 import handlers
 
-api = "xxx"
+api = "XXX"
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
@@ -12,12 +11,12 @@ dp.message_handler(commands=["start"])(handlers.start)
 dp.message_handler(text="Информация о боте")(handlers.info)
 dp.message_handler(text="Перевести")(handlers.translate_menu)
 dp.callback_query_handler(text="trans_bot")(handlers.trans_bot)
-dp.message_handler(state=Photo.photo_bot, content_types=types.ContentType.PHOTO)(handlers.translate_bot)
+dp.message_handler(state=handlers.Photo.photo_bot, content_types=types.ContentType.PHOTO)(handlers.translate_bot)
 dp.callback_query_handler(text="trans_giga")(handlers.trans_giga)
-dp.message_handler(state=Photo.photo_giga, content_types=types.ContentType.PHOTO)(handlers.translate_giga)
-dp.message_handler(state=Photo.photo_bot, content_types=[types.ContentType.TEXT
+dp.message_handler(state=handlers.Photo.photo_giga, content_types=types.ContentType.PHOTO)(handlers.translate_giga)
+dp.message_handler(state=handlers.Photo.photo_bot, content_types=[types.ContentType.TEXT
     , types.ContentType.STICKER])(handlers.handle_text)
-dp.message_handler(state=Photo.photo_giga, content_types=[types.ContentType.TEXT
+dp.message_handler(state=handlers.Photo.photo_giga, content_types=[types.ContentType.TEXT
     , types.ContentType.STICKER])(handlers.handle_text)
 
 if __name__ == "__main__":
